2d8b7813bc8f815966321f8eca796784a3619f36
David Noll <noll.david@gmail.com>
2018-03-23 16:15:52 -0400

    Initial Commit

---
+++ algStudy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algStudy
+//
+//  Created by David on 3/23/18.
+//  Copyright © 2018 davidmnoll. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
